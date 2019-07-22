4ddab70f024fb068f3ec93747d87f630a5d33c62
范优 <876255983@qq.com>
2019-07-22 21:51:20 +0800

    封装时间

--- kits.js
+++ kits.js
@@ -17,3 +17,45 @@ let kits = {};
 kits.randomInt = function (n, m) {
     return Math.floor(Math.random() * (m - n + 1) + n);
 }
+
+
+// /**
+//  * @description 封装一个数组中n-m的最大值
+//  * @param {string} n
+//  * @param {string}
+//  * @param {string}
+//  */
+
+// kits.max = function (n, m) {
+//     //假设数组中的第一项就是最大值
+//     var max = arr[0];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (max < arr[i]) {
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+
+
+/**
+ * @description 封装一个实时时间
+ * @param {} 
+ * @return 返回一个实时的时间
+ */
+
+kits.times = function () {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+}
